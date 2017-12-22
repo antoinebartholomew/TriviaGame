c88f92c3aa9431efd75d8b5a57151a4c72a89137
antoine bartholomew <antoinebartholomew@antoines-MacBook-Pro.local>
2017-12-21 22:39:55 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,10 @@
 // create variables for game
 var position = 0;
 var correctAnswer = 0;
-var trivia;
-var trivia_status;
-var question;
-var userSelectedChoice;
-var choices;
-var choiceA;
-var choiceB;
-var choiceC;
-// create a multimentional array of ..questions [index:0] along with the Choice A[index:1], Choice B [index:2], Choice C [index:3] and answer [index:4] for game
+var trivia, trivia_status;
+var question, userSelectedChoice;
+var choices, choiceA, choiceB, choiceC ;
+
 var questions =[
   ["What two jersey numbers did Kobe Bryant wear?" , "8 & 24", "10 & 30", "34 & 23", "A" ],
   ["What number did Micheal Jordan wear?" , "37", "23", "45", "B"],
@@ -17,30 +12,31 @@ var questions =[
   ["Who won more MVP awards?", "Steve Nash", "Kobe Bryant", "Allen Iverson", "A"  ],
   ["What year did LeBron James win his first NBA title in Cleveland?", "2015", "2013", "2011", "A"]
 ];
-function triviaReturn(x){
+function questionReturn(x){
   return document.getElementById(x);
 };
-function formulateQuestion(){
-        trivia = triviaReturn("trivia_question");
+function createQuestion(){
+        trivia = questionReturn("trivia_question");
         if(position >= questions.length){
           trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+ questions.length+" questions correct!</h2>";
-          triviaReturn("trivia_progress").innerHTML= "Final Score!";
+          questionReturn("trivia_progress").innerHTML= "Final Score!";
+          stop();  // call the stop function
           position = 0;
           correctAnswer = 0;
-          return false;
+          return;
         }
-        triviaReturn("trivia_progress").innerHTML = "Question " +(position+1)+" of "+questions.length;
+        questionReturn("trivia_progress").innerHTML = "Question " +(position+1)+" of "+questions.length;
         question = questions[position][0];
         choiceA = questions[position][1];
         choiceB = questions[position][2];
         choiceC = questions[position][3];
-        trivia_question.innerHTML = "<h3>"+question+"</h3>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'A'> "+choiceA+"<br>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'B'> "+choiceB+"<br>";
-        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'C'> "+choiceC+"<br>";
+        trivia_question.innerHTML = "<h3>"+question+"</h3><br><br>";
+        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'A'> "+choiceA+"<br><br><br>";
+        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'B'> "+choiceB+"<br><br><br>";
+        trivia_question.innerHTML += "<input type='radio' name ='choices' value = 'C'> "+choiceC+"<br><br><br>";
         trivia_question.innerHTML += "<button onclick= 'checkAnswer()'> Submit Answer </button>";
         if(position >= question.length){
-          trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+question.length+"question correct!</h2>"}
+          trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+question.length+"question correct!</h2> <br><br> "}
 };
 function checkAnswer(){
   choices = document.getElementsByName('choices');
@@ -53,33 +49,29 @@ function checkAnswer(){
     correctAnswer++;
   }
   position++;
-  formulateQuestion();
+  createQuestion();
 };
 
-var timeLeft = 3;
+var timeLeft = 15;
 var elem = document.getElementById('trivia_timer');
-var timerId = setInterval(countdown, 1000);
+var timer = setInterval(countdown, 1000);
 function countdown() {
   if (timeLeft == 0) {
     trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+ questions.length+" questions correct!</h2>";
-      triviaReturn("trivia_progress").innerHTML= "Final Score!";
-      position = 0;
-      correctAnswer = 0;
-      return false;
+      questionReturn("trivia_progress").innerHTML= "Final Score!";
+      //position = 0;
+
   } else {
     elem.innerHTML = timeLeft + ' seconds remaining';
     timeLeft--;
     //alert("Hello! I am an alert box!!");
   }
-}
+};
 
-// if(position >= questions.length){
-//   trivia_question.innerHTML = "<h2> You got "+correctAnswer+" of "+ questions.length+" questions correct!</h2>";
-//   triviaReturn("trivia_progress").innerHTML= "Final Score!";
-//   position = 0;
-//   correctAnswer = 0;
-//   return false;
-// }
+function stop() {
+ clearInterval(timer);
+ //createQuestion(x);
+};
 
 
-window.addEventListener("load", formulateQuestion, false)
+window.addEventListener("load", createQuestion(), false);
